f5697e52c14ddd70aa4b87b3147a4c416253b7a5
Andres Martinson <fossam@protonmail.com>
2018-04-22 18:55:00 -0600

    add a debug statement

--- pkg/pyinstaller-runtimehooks/pathopthubble.py
+++ pkg/pyinstaller-runtimehooks/pathopthubble.py
@@ -1,6 +1,4 @@
-# this runtimehook prepends paths to its environment so that the code
-# would always find the binaries that we ship in the package
-
 import os
 
 os.environ["PATH"] = '/opt/hubble/bin:/opt/hubble/libexec:' + os.environ["PATH"]
+print(os.environ["PATH"])
